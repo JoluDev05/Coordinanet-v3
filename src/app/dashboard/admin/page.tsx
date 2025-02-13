fe11210b2afe7f9e4ad6a6a91016ac4614e80612
Jorge Luis Arce <jorgearcedev1@gmail.com>
2025-02-13 11:34:44 -0700

    Insertando componentes_1

--- src/app/dashboard/admin/page.tsx
+++ src/app/dashboard/admin/page.tsx
@@ -1,8 +1,38 @@
+import AttendanceChart from "@/app/components/AttendanceChart";
+import CountChart from "@/app/components/CountChart";
+import UserCard from "@/app/components/UserCard";
 
 const AdminPage = () => {
   return (
-    <div className=''>AdminPage</div>
-  )
-}
+    <div className="p-4 flex gap-4 flex-col md:flex-row">
+      {/* LEFT */}
+      <div className="w-full lg:w-2/3 flex flex-col gap-8">
+        {/* User cards */}
+        <div className="flex gap-4 justify-between flex-wrap">
+          <UserCard type="Student" />
+          <UserCard type="teacher" />
+          <UserCard type="Coordinador" />
+          <UserCard type="staff" />
+        </div>
+        {/*Middle charts*/}
+        <div className="flex gap-4 flex-col lg:flex-row">
+          <div className="w-full lg:w-1/3 h-[450px]">
+            <CountChart />
+          </div>
+          {/*Attendance chart*/}
+          <div className="w-full lg:w-2/3 h-[450px]">
+          <AttendanceChart/>
+          </div>
+        </div>
+        {/*Bottom Chart*/}
+        <div className="w-full h-[450px] ">
 
-export default AdminPage
+        </div>
+      </div>
+      {/* RIGHT */}
+      <div className="w-full lg:w-1/3">r</div>
+    </div>
+  );
+};
+
+export default AdminPage;
